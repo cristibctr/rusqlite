be691b2f28a7bb88ba4dfa113761c040d977a55f
Cristian <cristian.bucataru@gmail.com>
2025-03-21 15:49:05 +0200

    add atomics and bulk-memory flags to libsqlite3-sys compilation

--- libsqlite3-sys/build.rs
+++ libsqlite3-sys/build.rs
@@ -123,6 +123,8 @@ mod build_bundled {
         println!("cargo:rerun-if-changed=sqlite3/wasm32-wasi-vfs.c");
         let mut cfg = cc::Build::new();
         cfg.file(format!("{lib_name}/sqlite3.c"))
+            .flag("-matomics")
+            .flag("-mbulk-memory")
             .flag("-DSQLITE_CORE")
             .flag("-DSQLITE_DEFAULT_FOREIGN_KEYS=1")
             .flag("-DSQLITE_ENABLE_API_ARMOR")
